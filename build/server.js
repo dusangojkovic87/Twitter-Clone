cffb7a5f0b42a526a05d11f3cab75881b027b118
Dusan Gojkovic <dusangojkovic087gmail.com>
2021-11-30 07:11:02 -0800

    dodata mongodb baza

--- build/server.js
+++ build/server.js
@@ -6,17 +6,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
 const homeRoutes_1 = __importDefault(require("./Routes/homeRoutes"));
 const PORT = 3000;
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use(body_parser_1.default.json());
-app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
-app.set('views', path_1.default.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
+app.set("views", path_1.default.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use(express_1.default.json());
 /*Routes*/
-app.use('/', homeRoutes_1.default);
-app.listen(PORT, () => {
-    console.log(`Server listens on ˘${PORT}`);
+app.use("/", homeRoutes_1.default);
+mongoose_1.default.connect('mongodb://localhost:27017/TwitterClone', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server listens on ˘${PORT}`);
+    });
+})
+    .catch((err) => {
+    throw err;
 });
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,35 @@
-import express from 'express';
-import path from 'path';
-import bodyParser from 'body-parser';
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import mongoose from 'mongoose';
+
 
 const app = express();
 
-import HomeRoutes from './Routes/homeRoutes';
+import HomeRoutes from "./Routes/homeRoutes";
 
 const PORT = 3000;
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.json());
 
-
-
 /*Routes*/
- app.use('/',HomeRoutes); 
+app.use("/", HomeRoutes);
+mongoose.connect('mongodb://localhost:27017/TwitterClone',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+.then(() =>{
+    app.listen(PORT, () => {
+        console.log(`Server listens on ˘${PORT}`);
+  });
+})
+.catch((err) =>{
+   throw err;
+})
 
-
-app.listen(PORT,() =>{
-    console.log(`Server listens on ˘${PORT}`);
-});
